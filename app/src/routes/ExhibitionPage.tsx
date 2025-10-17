0eeca3ede3e3abfffefa4d85b01494b8dee4655e
tracy-st <55551284+tracy-st@users.noreply.github.com>
2025-10-17 10:23:30 -0400

    Windsurf updates
    Exhibition works without styling

--- app/src/routes/ExhibitionPage.tsx
+++ app/src/routes/ExhibitionPage.tsx
@@ -1,10 +1,86 @@
-import 'exhibition-viewer';
-import '../App.css';
+// Hosted version.
+import { useEffect, useState } from "react";
+import { DelftExhibition } from "exhibition-viewer";
 
-export default function App() {
+import "../styles/index.css";
+import "../styles/lib.css";
+import { LocaleString } from "react-iiif-vault";
+import { DelftPresentation } from "exhibition-viewer";
+
+const search = new URLSearchParams(window.location.search);
+
+function ExhibitionPage() {
+  const [manifest, setManifest] = useState<any | null>(null);
+  const [collection, setCollection] = useState<any | null>(null);
+
+  const isPresentation = search.get("type") === "presentation";
+  const isEmbed = search.get("embed") === "true";
+  const manifestId = search.get("manifest");
+  const cutCorners = search.get("cut-corners");
+  const fullTitleBar = search.get("full-title-bar");
+
+  const options = {
+    cutCorners: !(cutCorners === "false"),
+    fullTitleBar: !(fullTitleBar === "false"),
+  };
+
+  useEffect(() => {
+    fetch("https://heritage.tudelft.nl/iiif/stores/manifest-editor/collection.json")
+      .then((r) => r.json())
+      .then((col) => {
+        setCollection(col);
+      });
+  }, []);
+
+  useEffect(() => {
+    if (manifestId) {
+      fetch(manifestId)
+        .then((response) => response.json())
+        .then(setManifest);
+    }
+  }, [manifestId]);
+
+  if (!collection) {
+    return null;
+  }
+
+  if (!manifest) {
+    return (
+      <div>
+        <ul className="my-8 w-full text-center">
+          {collection.items.map((item: any) => (
+            <li key={item.id} className="pb-4 text-2xl">
+              <a href={`?manifest=${item.id}`} className="hover:underline">
+                <LocaleString>{item.label}</LocaleString>
+              </a>{" "}
+              (
+              <a href={`?manifest=${item.id}&type=presentation`} className="hover:underline">
+                Presentation
+              </a>
+              )
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+
   return (
-    <div>
-      <exhibition-viewer manifest="http://leedsunilibrary.exhibitionviewer.org/iiif/marie-hartley.json"></exhibition-viewer>
+    <div className="flex w-full flex-col items-center">
+      <div className="min-h-[90vh] w-full max-w-screen-xl px-5 py-10 lg:px-10">
+        {isPresentation ? (
+          <div className="h-[800px]">
+            {isEmbed ? (
+              <iframe src={`/embed.html?manifest=${manifestId}`} className="h-full w-full" title="Presentation" />
+            ) : (
+              <DelftPresentation manifest={manifest} options={options} language="en" viewObjectLinks={[]} />
+            )}
+          </div>
+        ) : (
+          <DelftExhibition manifest={manifest} options={options} language="en" viewObjectLinks={[]} />
+        )}
+      </div>
     </div>
   );
 }
+export default ExhibitionPage;
